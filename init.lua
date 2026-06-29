b29d148d890e36526e3d86b50ddd30f331a2f63d
Sheriff-Unit-3 <210896603+Sheriff-Unit-3@users.noreply.github.com>
2026-06-28 21:11:35 -0500

    minetest -> core

--- init.lua
+++ init.lua
@@ -3,21 +3,19 @@
 --
 -- Textures borrowed/derived from Mossmanikin's alternate trunks mod
 
-local trees={
-	{ "default:tree",        "round_default_tree_top.png" },
-	{ "default:jungletree",  "default_jungletree_top.png" },
-	{ "default:pine_tree",   "round_pine_tree_top.png"    },
-	{ "default:acacia_tree", "round_acacia_tree_top.png"  },
-	{ "default:aspen_tree",  "round_aspen_tree_top.png"   },
-	{ "default:cactus",      "default_cactus_top.png"     }
+local trees = {
+	{"default:tree",        "round_default_tree_top.png"},
+	{"default:jungletree",  "default_jungletree_top.png"},
+	{"default:pine_tree",   "round_pine_tree_top.png"   },
+	{"default:acacia_tree", "round_acacia_tree_top.png" },
+	{"default:aspen_tree",  "round_aspen_tree_top.png"  },
+	{"default:cactus",      "default_cactus_top.png"    }
 }
-
-local trees2 = {}
 
 for _,tree in pairs(trees) do
 	local nodename, top = unpack(tree)
 
-	local oldnode = minetest.registered_nodes[nodename]
+	local oldnode = core.registered_nodes[nodename]
 	if not oldnode then
 		error("[round_trunks] "..nodename.." is not a node.")
 	end
@@ -27,7 +25,7 @@ for _,tree in pairs(trees) do
 		def[i] = rawget(oldnode, i)
 	end
 
-	minetest.override_item(nodename, {
+	core.override_item(nodename, {
 		drawtype = "mesh",
 		mesh = "round_trunks_mesh.obj",
 		tiles = {top, top, def.tiles[3]},
@@ -36,33 +34,32 @@ for _,tree in pairs(trees) do
 
 	trees2[#trees2+1] = nodename
 
-	minetest.register_node(":"..nodename.."_cube", def)
+	core.register_node(":"..nodename.."_cube", def)
 
-
-	minetest.register_craft({
+	core.register_craft({
 		output = nodename.." 4",
 		recipe = {
-			{ def.name, def.name },
-			{ def.name, def.name }
+			{def.name, def.name},
+			{def.name, def.name}
 		}
 	})
 
-	minetest.register_craft({
+	core.register_craft({
 		output = def.name.." 4",
 		recipe = {
-			{ nodename, nodename },
-			{ nodename, nodename }
+			{nodename, nodename},
+			{nodename, nodename}
 		}
 	})
 end
 
--- [[ This lbm should fix black trunks.
-minetest.register_lbm({
+--[[ This lbm should fix black trunks.
+core.register_lbm({
 	name = "round_trunks:trunkfix",
 	nodenames = trees2,
 	action = function(pos, node)
 		node.param1 = nil
-		minetest.set_node(pos, node)
+		core.set_node(pos, node)
 	end
 })
 --]]
